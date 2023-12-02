12ac982da005b552decebe6ef3785f697acab7aa
IDEDARY <IDEDARY@gmail.com>
2023-12-02 22:15:45 +0100

    tween fix

--- src/tween.rs
+++ src/tween.rs
@@ -16,20 +16,20 @@ pub trait Tween {
     /// Function for linear interpolation between 2 values.
     /// 
     /// * `slide` ranges from 0.0 to 1.0
-    fn tween(&mut self, other: &Self, slider: f32);
+    fn tween(&mut self, value_1: &Self, value_2: &Self, slider: f32);
 }
 
 
 impl Tween for f32 {
-    fn tween(&mut self, other: &Self, slider: f32) {
-        *self += (*other - *self) * slider;
+    fn tween(&mut self, value_1: &Self, value_2: &Self, slider: f32) {
+        *self = value_1 + (*value_2 - *value_1) * slider;
     }
 }
 
 #[cfg(feature = "bevy")]
 impl Tween for Vec2 {
-    fn tween(&mut self, other: &Self, slider: f32) {
-        self.x.tween(&other.x, slider);
-        self.y.tween(&other.y, slider);
+    fn tween(&mut self, value_1: &Self, value_2: &Self, slider: f32) {
+        self.x.tween(&value_1.x, &value_2.x, slider);
+        self.y.tween(&value_1.y, &value_2.y, slider);
     }
 }
