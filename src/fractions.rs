c4a95c18453ea59141a465c7be08f46f2a20b3de
IDEDARY <IDEDARY@gmail.com>
2023-12-02 21:22:55 +0100

    fixed and added bevy impl

--- src/fractions.rs
+++ src/fractions.rs
@@ -1,7 +1,11 @@
 use std::{ops::{Add, AddAssign, Sub, SubAssign, Mul, MulAssign, Div, DivAssign}, fmt::{Display, self}};
 use core::cmp::{PartialEq, PartialOrd, Ordering, Eq};
+#[cfg(feature = "deku")]
 use deku::{DekuRead, DekuWrite, DekuContainerWrite, DekuUpdate};
+#[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
+#[cfg(feature = "speedy")]
+use speedy::{Readable, Writable};
 
 
 const PRIMES: [i32; 100] = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541];
@@ -25,7 +29,10 @@ const PRIMES: [i32; 100] = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 
 /// assert_eq!(frac_1 / frac_2, Fraction::new(4, 3));
 /// assert_eq!(frac_1 >= frac_2, true);
 /// ```
-#[derive(Debug, Eq, Copy, Clone, DekuRead, DekuWrite, Deserialize, Serialize)]
+#[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
+#[cfg_attr(feature = "deku", derive(DekuRead, DekuWrite))]
+#[cfg_attr(feature = "deku", derive(Writable, Readable))]
+#[derive(Debug, Default, Eq, Copy, Clone)]
 pub struct Fraction {
     pub numerator: i32,
     pub denominator: i32,
--- src/tween.rs
+++ src/tween.rs
@@ -1,3 +1,6 @@
+#[cfg(feature = "bevy")]
+use bevy::prelude::Vec2;
+
 /// # Tween
 /// Very simple function for linear interpolation between 2 values.
 /// 
@@ -14,4 +17,19 @@ pub trait Tween {
     /// 
     /// * `slide` ranges from 0.0 to 1.0
     fn tween(&mut self, other: &Self, slider: f32);
+}
+
+
+impl Tween for f32 {
+    fn tween(&mut self, other: &Self, slider: f32) {
+        *self += (*other - *self) * slider;
+    }
+}
+
+#[cfg(feature = "bevy")]
+impl Tween for Vec2 {
+    fn tween(&mut self, other: &Self, slider: f32) {
+        self.x.tween(other.x, slider);
+        self.y.tween(other.y, slider);
+    }
 }
