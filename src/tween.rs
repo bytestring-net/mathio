23119aca3754d29113d99539a6d09d515eac0f6f
IDEDARY <IDEDARY@gmail.com>
2023-12-02 20:08:39 +0100

    tween trait + version 0.2.2

--- src/tween.rs
+++ src/tween.rs
@@ -6,3 +6,13 @@ pub fn tween(value_1: f32, value_2: f32, slider: f32) -> f32 {
     let diff = value_2 - value_1;
     value_1 + diff * slider
 }
+
+/// # Tween Trait
+/// Trait for implementing tweening logic for data types.
+pub trait Tween {
+    /// # Tween
+    /// Function for linear interpolation between 2 values.
+    /// 
+    /// * `slide` ranges from 0.0 to 1.0
+    fn tween(&mut self, other: &Self, slider: f32);
+}
