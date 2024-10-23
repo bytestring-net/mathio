22cb1aa416e678e8e584b53ab084df4d8c90cd7f
IDEDARY <IDEDARY@gmail.com>
2024-10-23 17:54:32 +0200

    sync

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,9 @@ pub use crate::period::*;
 fn test() {
     let a = Frac32::ONE * Frac32::new(1, 2);
     assert_eq!(a, Frac32::new(1, 2));
+    println!("{a}");
 
     let a = Frac32::new(1, 6) + Frac32::new(1, 4);
     assert_eq!(a, Frac32::new(5, 12));
+    println!("{a}");
 }
